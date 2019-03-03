0613886a0866864ceea1f1b938e03bc095ee58e0
Danny <3508879442@qq.com>
2019-03-03 09:25:19 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 调用Element UI组件 -->
+    <div class="login">
+      <h3>登录</h3>账号
+      <el-input placeholder="请输入内容" v-model="input10" clearable></el-input>密码
+      <el-input aria-placeholder="请输入密码" v-model="input11" show-password></el-input>
+      <el-row>
+        <el-button type="primary">提交</el-button>
+        <el-button>重置</el-button>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      input10: "",
+      input11: ""
+    };
+  },
+  // 请求数据
+  mounted() {
+    this.$axios.get("https://api.github.com/users").then(res => {
+      console.log(res.data);
+    });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.login {
+  width: 300px;
+  height: 200px;
+  margin: 0 auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//1.引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 引入axios 第一步
+import axios from 'axios';
+
+
+
+
+//3.全局注册组件
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
+
+// 把axios绑定到vue实例的属性$axios 第二步
+Vue.prototype.$axios=axios;
 
 new Vue({
   render: h => h(App),
